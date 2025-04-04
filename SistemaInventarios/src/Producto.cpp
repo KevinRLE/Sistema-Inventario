16e4df2a18cbfce427db89da760e78864d3ab1f8
Anthony Suc <asucg@miumg.edu.gt>
2025-04-03 20:18:40 -0600

    Implementacion Clase Producto

---
+++ SistemaInventarios/src/Producto.cpp
@@ -0,0 +1,5 @@
+#include "Producto.h"
+
+// Constructor incluido por Anthony Suc
+Producto::Producto(int _codigo, std::string _nombre, float _precio, int _stock)
+    : codigo(_codigo), nombre(_nombre), precio(_precio), stock(_stock) {}
