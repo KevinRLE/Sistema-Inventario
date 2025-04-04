d23bb083bbe7a4793831a46f28411f6933daae22
Marco Hernandez <mhernandezt23@miumg.edu.gt>
2025-04-03 20:26:53 -0600

    Implementacion Producto.h by Marco Hernandez

--- SistemaInventarios/include/Producto.h
+++ SistemaInventarios/include/Producto.h
@@ -1,16 +1,16 @@
+//Implementacion Marco H. 03/04/2025
 #ifndef PRODUCTO_H
 #define PRODUCTO_H
 
+#include <string>
 
-class Producto
-{
-    public:
-        Producto();
-        virtual ~Producto();
+class Producto {
+public:
+    int codigo;
+    std::string nombre;
+    float precio;
+    int stock;
+    std::string tipo;
 
-    protected:
-
-    private:
+    Producto(int _codigo, std::string _nombre, float _precio, int _stock, std::string _tipo);
 };
-
-#endif // PRODUCTO_H
